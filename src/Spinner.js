88db1a2fec17250a623d349591bfaef985664c83
smith <smith220@gmail.com>
2022-12-27 15:04:03 -0800

    main project

---
+++ src/Spinner.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Spinner = (props)=>{
+  return (
+    <diV className="ui active dimmer">
+<div className="ui big text loader">
+    Loading....
+</div>
+    </diV>
+  )  
+}
+Spinner.defultProps ={
+    message:"loading...."
+}
+export default Spinner
