4834a9be7234dcd529d7749b1ef5cf38ff6fb19e
smith <smith220@gmail.com>
2022-12-27 15:11:33 -0800

    test

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,6 +1,9 @@
 import React from "react";
+
 const Spinner = (props)=>{
+const smith = 'testing commith'
   return (
+  
     <diV className="ui active dimmer">
 <div className="ui big text loader">
     Loading....
